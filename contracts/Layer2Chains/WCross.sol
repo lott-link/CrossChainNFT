29f216d081019a09a4e58a02d5da2ee01710397b
Renope <r.nouripoor@gmail.com>
2023-09-29 00:05:41 +0330

    edits

--- contracts/Layer2Chains/WCross.sol
+++ contracts/Layer2Chains/WCross.sol
@@ -1,21 +1,63 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "./WNFT.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
 import "./utils/Swapper.sol";
 import "./utils/Burner.sol";
 
-contract Ethereum_Cross_NFT is Swapper, Burner, WNFT {
+contract Ethereum_Cross_NFT is Swapper, Burner, ERC721Burnable, CCIPReceiver {
+    address immutable i_link;
+    uint64 currentSelector;
+    uint64 targetSelector;
+    uint256 counter;
+
+    struct WrappedToken {
+        address contAddr;
+        uint256 tokenId;
+        string name;
+        string symbol;
+        string uri;
+    }
+    mapping(uint256 => WrappedToken) wrappedTokens;
 
     constructor(
         uint64 _currentSelector,
         uint64 _targetSelector,
         address router,
         address link
-    ) WNFT(router, link) {
+    ) ERC721("Ethereum_Wrapped_NFT", "wNFT") CCIPReceiver(router) {
+        i_link = link;
         currentSelector = _currentSelector;
         targetSelector = _targetSelector;
         LinkTokenInterface(i_link).approve(i_router, type(uint256).max);
+    }
+
+    function tokenURI(
+        uint256 wTokenId
+    ) public view override returns (string memory) {
+        _requireMinted(wTokenId);
+        return (wrappedTokens[wTokenId].uri);
+    }
+
+    function tokenInfo(uint256 wTokenId) public view returns(
+        string memory sourceChain,
+        address contAddr,
+        uint256 tokenId,
+        string memory name,
+        string memory symbol,
+        string memory uri
+    ) {
+        sourceChain = "Ethereum";
+        WrappedToken memory wToken = wrappedTokens[wTokenId];
+        contAddr = wToken.contAddr;
+        tokenId = wToken.tokenId;
+        name = wToken.name;
+        symbol = wToken.symbol;
+        uri = wToken.uri;
     }
 
     function getFee(
@@ -76,6 +118,15 @@ contract Ethereum_Cross_NFT is Swapper, Burner, WNFT {
         );
     }
 
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public pure override(CCIPReceiver, ERC721) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+
+// internal functions ------------------------------------------------------------------------------
+
     function _ccipReceive(
         Client.Any2EVMMessage memory message
     ) internal virtual override {
@@ -90,9 +141,50 @@ contract Ethereum_Cross_NFT is Swapper, Burner, WNFT {
             uint256 tokenId,
             string memory name,
             string memory symbol,
-            string memory tokenURI
+            string memory uri
         ) = abi.decode(message.data, (address, address, uint256, string, string, string));
-        wMint(to, contAddr, tokenId, name, symbol, tokenURI);
+        _wMint(to, contAddr, tokenId, name, symbol, uri);
+    }
+    
+    function _wMint(
+        address userAddr,
+        address contAddr,
+        uint256 tokenId,
+        string memory _name,
+        string memory _symbol,
+        string memory _uri
+    ) internal {
+        uint256 wTokenId = ++counter;
+        wrappedTokens[wTokenId] = WrappedToken(
+            contAddr,
+            tokenId,
+            _name,
+            _symbol,
+            _uri
+        );
+        _safeMint(userAddr, wTokenId);
     }
 
+    function _getFee(
+        address contAddr,
+        address userAddr,
+        uint256 tokenId,
+        bool payInLink
+    ) internal view returns (uint256 fee) {
+        // Create an EVM2AnyMessage struct in memory with necessary information for sending a cross-chain message
+        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
+            receiver: abi.encode(address(this)),
+            data: abi.encode(
+                contAddr,
+                userAddr,
+                tokenId
+            ),
+            tokenAmounts: new Client.EVMTokenAmount[](0),
+            extraArgs: "",
+            feeToken: payInLink ? i_link : address(0)
+        });
+
+        // Get the fee required to send the message
+        fee = IRouterClient(i_router).getFee(targetSelector, message);
+    }
 }
