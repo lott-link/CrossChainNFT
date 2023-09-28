c713a66ab8f6f2bb9455a33f7f4f67b7f9bfa7b3
Renope <r.nouripoor@gmail.com>
2023-09-28 22:38:14 +0330

    comments added

--- contracts/MainChain/Cross.sol
+++ contracts/MainChain/Cross.sol
@@ -9,22 +9,29 @@ import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/interfaces/LinkT
 import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
 
 /**
- * NFT Cross-Chain Bridge to Polygon Powered By CCIP V1
- * is one of the safest ways to bridge NFT From Ethereum to Polygon.
- * For using this Bridge :
+ * @title NFT Cross-Chain Bridge to Polygon Powered By CCIP V1
+ * @dev This smart contract provides a secure bridge for transferring NFTs from Ethereum to Polygon.
+ * To use this bridge:
  * a. Approve your NFT to the address of this contract.
- * b. Approve your Link token to this contract or Check the Xcall fee and add Eth value to C.
- * c. Call the “requestTransferCrossChain” Function and add data.
- *   -Your NFT will Lock in this contract.
- *   -around 30 min your NFT will mint on the same contract address on Polygon to the to address.
- *   -you can find the CCIP transaction ID on Logs.
- *   -you Polygon NFT owned this NFT and it is transferable.
+ * b. Approve your Link token to this contract or provide ETH for the transfer fee.
+ * c. Call the `requestTransferCrossChain` function with the required data to initiate the transfer process.
+ *    - Your NFT will be locked in this contract.
+ *    - After approximately 30 minutes, your NFT will be minted on the Polygon network at the specified address.
+ *    - You can find the CCIP transaction ID in the contract logs.
+ *    - Your Polygon NFT will own the original NFT and will be transferable.
  */
 contract Polygon_NFT_Bridge is ERC721Holder, CCIPReceiver {
     address immutable i_link;
     uint64 currentSelector;
     uint64 targetSelector;
 
+    /**
+     * @dev Constructor to initialize the bridge contract.
+     * @param _currentSelector The current chain selector.
+     * @param _targetSelector The target chain selector.
+     * @param router The address of the CCIP router contract.
+     * @param link The address of the Link token contract.
+     */
     constructor(
         uint64 _currentSelector,
         uint64 _targetSelector,
@@ -37,6 +44,14 @@ contract Polygon_NFT_Bridge is ERC721Holder, CCIPReceiver {
         LinkTokenInterface(link).approve(i_router, type(uint256).max);
     }
 
+    /**
+     * @dev Get the fee required for a cross-chain NFT transfer.
+     * @param userAddr The address of the user initiating the transfer.
+     * @param contAddr The address of the NFT contract.
+     * @param tokenId The ID of the NFT being transferred.
+     * @param payInLink True if the fee should be paid in Link tokens; false if it should be paid in ETH.
+     * @return fee The fee amount required for the transfer.
+     */
     function getFee(
         address userAddr,
         address contAddr,
@@ -64,6 +79,17 @@ contract Polygon_NFT_Bridge is ERC721Holder, CCIPReceiver {
         fee = IRouterClient(i_router).getFee(targetSelector, message);
     }
 
+    /**
+     * @dev Request the cross-chain transfer of an NFT.
+     * @param contAddr The address of the NFT contract.
+     * @param to The address where the NFT will be minted on the Polygon network.
+     * @param tokenId The ID of the NFT being transferred.
+     * @param dappAddr The address of the dapp initiating the transfer.
+     * @notice To initiate the NFT transfer, you have two options:
+     * 1. Pay the transfer fee in ETH by sending ETH along with this function call.
+     * 2. Approve the Link token for transfer to this contract using the Link token's approval function.
+     * Please ensure you have enough ETH or approved Link tokens to cover the transfer fee.
+     */
     function requestTransferCrossChain(
         address contAddr,
         address to,
@@ -119,6 +145,13 @@ contract Polygon_NFT_Bridge is ERC721Holder, CCIPReceiver {
         }
     }
 
+
+    /**
+     * @dev Internal function to release an NFT to the specified recipient.
+     * @param contAddr The address of the NFT contract.
+     * @param to The address where the NFT will be minted on the Polygon network.
+     * @param tokenId The ID of the NFT being transferred.
+     */
     function _release(
         address contAddr,
         address to,
@@ -128,6 +161,10 @@ contract Polygon_NFT_Bridge is ERC721Holder, CCIPReceiver {
         NFT.safeTransferFrom(address(this), to, tokenId);
     }
 
+    /**
+     * @dev Internal function to handle the receipt of a cross-chain message.
+     * @param message The cross-chain message received.
+     */
     function _ccipReceive(
         Client.Any2EVMMessage memory message
     ) internal virtual override {
